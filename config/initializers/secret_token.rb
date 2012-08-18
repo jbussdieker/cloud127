dd08e5dac1ca409e76949c353a30afc6edaa96e2
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-08-18 16:18:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloud127::Application.config.secret_token = '651ddb999f13bdf9f674a007cb7229c753d4d82fa3eddf1023f83ecec03395e497a10f5c4f40e233461be99925774922fa2f532b89765e2b53a611f4a4797f9a'
