1f8b3c40e310d071e119733fe8a2366f4e821709
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-08-19 20:03:22 -0700

    Cleanup

--- app/helpers/vms_helper.rb
+++ app/helpers/vms_helper.rb
@@ -2,6 +2,7 @@ module VmsHelper
   def status_badge status
     return content_tag(:b, status, class: "badge") if status == "stopped"
     return content_tag(:b, status, class: "badge badge-info") if status == "creating"
+    return content_tag(:b, status, class: "badge badge-warning") if status == "starting"
     return content_tag(:b, status, class: "badge badge-success") if status == "running"
   end
 end
--- app/workers/vm_worker.rb
+++ app/workers/vm_worker.rb
@@ -2,12 +2,11 @@ class VmWorker
   include Sidekiq::Worker
 
   def perform(vm_id)
-    puts 'Doing hard work'
     vm = Vm.find(vm_id)
     vm.uuid = Cloud127::Vm.clone(vm.template_uuid, vm.name)
-    vm.status = "stopped"
-    vm.image = "false"
+    vm.status = "starting"
+    vm["Image"] = "false"
     vm.save
-    puts 'Hard work done!'
+    vm.start
   end
 end
